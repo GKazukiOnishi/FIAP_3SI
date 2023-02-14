f95c6f413ac6ac6ef1e81ab63e4ee876d55e8abd
Logon Aluno <logonrmlocal@fiap.com.br>
2023-02-14 20:44:52 -0300

    Anotações e Códigos da Aula de 02/14/2023

--- programming_and_database_management/subconsultas/Capítulo 01 - Subconsultas(Exemplos).sql
+++ programming_and_database_management/subconsultas/Capítulo 01 - Subconsultas(Exemplos).sql
@@ -36,7 +36,7 @@ HAVING   MIN(salary) >
 -------------------------------------------------
 SELECT employee_id, last_name
 FROM   employees
-WHERE  salary =
+WHERE  salary IN
                 (SELECT   MIN(salary)
                  FROM     employees
                  GROUP BY department_id);
@@ -47,7 +47,7 @@ FROM   employees
 WHERE  job_id =
                 (SELECT job_id
                  FROM   employees
-                 WHERE  last_name = 'Haas');
+                 WHERE  last_name = 'Haas'); --uma subquery retornando null pode anular toda a query principal, mesmo que o resto rode
 
 -------------------------------------------------
 SELECT employee_id, last_name, job_id, salary
@@ -57,14 +57,28 @@ WHERE  salary < ANY
                      FROM   employees
                      WHERE  job_id = 'IT_PROG')
 AND    job_id <> 'IT_PROG';
+--melhor
+SELECT employee_id, last_name, job_id, salary
+FROM   employees
+WHERE  salary < (SELECT MAX(salary)
+                 FROM   employees
+                 WHERE  job_id = 'IT_PROG')
+AND    job_id <> 'IT_PROG';
 
 -------------------------------------------------
 SELECT employee_id, last_name, job_id, salary
 FROM   employees
-WHERE  salary < ANY
+WHERE  salary < ALL
                     (SELECT salary
                      FROM   employees
                      WHERE  job_id = 'IT_PROG')
+AND    job_id <> 'IT_PROG';
+
+SELECT employee_id, last_name, job_id, salary
+FROM   employees
+WHERE  salary < (SELECT MIN(salary)
+                 FROM   employees
+                 WHERE  job_id = 'IT_PROG')
 AND    job_id <> 'IT_PROG';
 
 -------------------------------------------------
@@ -72,5 +86,12 @@ SELECT emp.last_name
 FROM   employees emp
 WHERE  emp.employee_id NOT IN
                            (SELECT mgr.manager_id
-                            FROM   employees mgr);
+                            FROM   employees mgr
+                            WHERE  mgr.manager_id IS NOT NULL); --validar o NOT NULL resolve para exibir, mas está errado
+-- o de cima retorna funcionários que não são managers, porém dentre os managers temos aqueles que são subordinados de outros
+-- o de baixo retorna funcionários que são subordinados
+--melhor e correto assim
+SELECT e.last_name
+FROM   employees e
+WHERE  e.manager_id IS NOT NULL;
 -------------------------------------------------
