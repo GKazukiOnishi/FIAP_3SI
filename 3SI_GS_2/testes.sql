a2dbf48d72a85ac8cf927447afdd0585f0615c11
GKazukiOnishi <kazuki.onishi@hotmail.com>
2023-11-15 15:23:21 -0300

    Global PLSQL

---
+++ 3SI_GS_2/testes.sql
@@ -0,0 +1,43 @@
+/*
+DROP INDEX idx_dia_trc_las_cha_date;
+
+CREATE INDEX idx_dia_trc_las_cha_date
+ON diabetes(TRUNC(last_change_date));*/
+
+SELECT encounter_id
+FROM   diabetes
+WHERE  TRUNC(last_change_date) = TO_DATE('05/11/2023','DD/MM/YYYY')
+AND    time_in_hospital + 1 < 14;
+SELECT encounter_id
+FROM   diabetes
+WHERE  TRUNC(last_change_date) = TO_DATE('06/11/2023','DD/MM/YYYY')
+AND    time_in_hospital + 1 < 14;
+SELECT encounter_id
+FROM   diabetes
+WHERE  TRUNC(last_change_date) = TO_DATE('07/11/2023','DD/MM/YYYY')
+AND    time_in_hospital + 1 < 14;
+SELECT encounter_id
+FROM   diabetes
+WHERE  TRUNC(last_change_date) = TO_DATE('08/11/2023','DD/MM/YYYY')
+AND    time_in_hospital + 1 < 14;
+SELECT encounter_id
+FROM   diabetes
+WHERE  TRUNC(last_change_date) = TO_DATE('09/11/2023','DD/MM/YYYY')
+AND    time_in_hospital + 1 < 14;
+SELECT encounter_id
+FROM   diabetes
+WHERE  TRUNC(last_change_date) = TO_DATE('10/11/2023','DD/MM/YYYY')
+AND    time_in_hospital + 1 < 14;
+SELECT encounter_id
+FROM   diabetes
+WHERE  TRUNC(last_change_date) = TO_DATE('11/11/2023','DD/MM/YYYY')
+AND    time_in_hospital + 1 < 14;
+SELECT encounter_id
+FROM   diabetes
+WHERE  TRUNC(last_change_date) = TO_DATE('12/11/2023','DD/MM/YYYY')
+AND    time_in_hospital + 1 < 14;
+
+/*
+SELECT MIN(last_change_date)
+FROM   diabetes
+WHERE  TRUNC(last_change_date) = TO_DATE('10/11/2023','DD/MM/YYYY');*/
