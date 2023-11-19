4ec7061157e6010f8efae95da852f7fc6db34b2e
GKazukiOnishi <kazuki.onishi@hotmail.com>
2023-11-19 16:08:01 -0300

    Finalizando ajustes

--- 3SI_GS_2/Micro/api/ods3API/bin/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('../src/app');
-const port = process.env.PORT || '3000';
-
-app.listen(port, function () {
-  console.log(`app listening on port ${port}`)
-})
--- 3SI_GS_2/Micro/api/ods3API/src/controllers/objetivoController.js
+++
@@ -1,8 +0,0 @@
-const objetivoDao = require('../dao/objetivoDao');
-const config = require('../config/databaseConfig');
-
-exports.get = async (req, res, next) => {
-  const result = await objetivoDao.getObjetivos(req.query?.page);
-
-  res.status(200).header("page", result.page).header("total", result.total).header("perPage", config.listPerPage).send(result.data);
-};
