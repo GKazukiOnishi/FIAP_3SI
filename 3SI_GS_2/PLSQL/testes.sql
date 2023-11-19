1bd5b768a34360aecf3764d8d80ef3333a71fbcc
GKazukiOnishi <kazuki.onishi@hotmail.com>
2023-11-19 17:31:21 -0300

    Ajustes na global de pl

--- 3SI_GS_2/PLSQL/testes.sql
+++ 3SI_GS_2/PLSQL/testes.sql
@@ -4,35 +4,35 @@ DROP INDEX idx_dia_trc_las_cha_date;
 CREATE INDEX idx_dia_trc_las_cha_date
 ON diabetes(TRUNC(last_change_date));*/
 
-SELECT encounter_id
+SELECT COUNT(*)
 FROM   diabetes
 WHERE  TRUNC(last_change_date) = TO_DATE('05/11/2023','DD/MM/YYYY')
 AND    time_in_hospital + 1 < 14;
-SELECT encounter_id
+SELECT COUNT(*)
 FROM   diabetes
 WHERE  TRUNC(last_change_date) = TO_DATE('06/11/2023','DD/MM/YYYY')
 AND    time_in_hospital + 1 < 14;
-SELECT encounter_id
+SELECT COUNT(*)
 FROM   diabetes
 WHERE  TRUNC(last_change_date) = TO_DATE('07/11/2023','DD/MM/YYYY')
 AND    time_in_hospital + 1 < 14;
-SELECT encounter_id
+SELECT COUNT(*)
 FROM   diabetes
 WHERE  TRUNC(last_change_date) = TO_DATE('08/11/2023','DD/MM/YYYY')
 AND    time_in_hospital + 1 < 14;
-SELECT encounter_id
+SELECT COUNT(*)
 FROM   diabetes
 WHERE  TRUNC(last_change_date) = TO_DATE('09/11/2023','DD/MM/YYYY')
 AND    time_in_hospital + 1 < 14;
-SELECT encounter_id
+SELECT COUNT(*)
 FROM   diabetes
 WHERE  TRUNC(last_change_date) = TO_DATE('10/11/2023','DD/MM/YYYY')
 AND    time_in_hospital + 1 < 14;
-SELECT encounter_id
+SELECT COUNT(*)
 FROM   diabetes
 WHERE  TRUNC(last_change_date) = TO_DATE('11/11/2023','DD/MM/YYYY')
 AND    time_in_hospital + 1 < 14;
-SELECT encounter_id
+SELECT COUNT(*)
 FROM   diabetes
 WHERE  TRUNC(last_change_date) = TO_DATE('12/11/2023','DD/MM/YYYY')
 AND    time_in_hospital + 1 < 14;
